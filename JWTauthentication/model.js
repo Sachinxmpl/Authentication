bb1780db879ae8bdab5c15bd00a07e1d3e7b0aec
Sachin K.C. <sachinxpml6@gmail.com>
2024-08-17 13:22:35 -0700

    JWT auth

--- JWTauthentication/model.js
+++ JWTauthentication/model.js
@@ -1,18 +1,27 @@
-const {Schema , model} = require("mongoose")
+const { Schema, model } = require("mongoose")
+const bcrypt = require("bcrypt")
 
 const userSchema = new Schema({
-    username : {
-        type : String , 
-        required : true ,
-        unique : true 
-    } , 
-    password : {
-        type : String , 
-        required : true ,
-        unique : true 
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
     }
-} , {
-    timestamps : true , 
+}, {
+    timestamps: true,
 })
 
-module.exports = model("User" , userSchema)
+userSchema.pre("save", async function (next) {
+    console.log("inside pre-middleware")
+    if (!this.isModified("password")) return next()
+    const salt = await bcrypt.genSalt()
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+
+module.exports = model("User", userSchema)
--- JWTauthentication/server.js
+++ JWTauthentication/server.js
@@ -4,18 +4,41 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require("express")
 const app = express()
+const cookieParser = require("cookie-parser")
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
 const connectDb = require("./connectdb");
 const User = require("./model.js")
 const { v4: uuidv4 } = require("uuid")
-const { setUser, getUser } = require("./auth.js")
+
 
 connectDb(process.env.DB_URI)
 
 
+app.use(cookieParser())
 app.use(express.json())
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }))
+
+
+const verifyToken = (req,res,next) =>{
+    const token = req.cookies.token 
+    if(!token){
+        return res.redirect("/")
+    }
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded.username
+        return next()
+    }catch(err){
+        return res.redirect("/")
+    }
+}
+
+
+
+
 
 app.get("/", (req, res) => {
     res.render("index.ejs")
@@ -30,33 +53,64 @@ app.get("/register", (req, res) => {
 })
 
 app.get("/dashboard", (req, res) => {
+    res.locals.username = req.user 
+    console.log(req.user)
     res.render("dashboard.ejs")
 })
 
-app.get("/secretpage", (req, res) => {
+app.get("/secretpage",verifyToken ,  (req, res) => {
     res.render("secretpage.ejs")
 })
 
 app.post("/register", async (req, res) => {
     const { username, password } = req.body;
-    await User.create({
-        username,
-        password
+    const user = new User({ username, password })
+    await user.save()
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET)
+    res.cookie("token", token, {
+        httpOnly: true
     })
     return res.redirect("/dashboard")
 })
 
+app.post("/logout" , (req,res)=>{
+    console.log(req.user)
+    res.clearCookie("token")
+    return res.redirect("/")
+})
+
+
 app.post("/login", async (req, res) => {
     const { username, password } = req.body
-    const user = await User.findOne({ username, password })
+    const user = await User.findOne({ username})
     if (!user) {
         return res.json({
-            error: 'Invalid user '
+            error: 'Something went wrong'
         })
     }
-    const sessionId = uuidv4()
-    return res.redirect("/dashboard")
+
+    bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+            return res.json({
+                error: 'Something went wrong'
+            })
+        }
+        if (result) {
+            const token = jwt.sign({ username }, process.env.JWT_SECRET)
+            res.cookie("token", token, {
+                httpOnly: true
+            })
+            return res.redirect("/dashboard")
+        }
+        return res.json({
+            error: 'Something went wrong'
+        })
+    })
 })
+
+
+
 
 app.listen("4000", () => {
     console.log("server started")
